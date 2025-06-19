f45330a03ea315ef76a39d52ff15caa2556d72d9
Krish Agarwal <krishagarwal7080@gmail.com>
2025-06-19 19:44:21 +0530

    smart pointer added for safe memory play

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 
 #include <iostream>
 #include <string>
+#include <memory>  
 
 int main()
 {
@@ -11,7 +12,8 @@ int main()
     bool isAIPlayer = false;
     std::string player1name, player2name;
     char mv1 = 'X', mv2 = 'O';
-    Player *p1, *p2;
+    std::unique_ptr<Player> p1;
+    std::unique_ptr<Player> p2;
 
     while (true)
     {
@@ -19,60 +21,57 @@ int main()
         std::cout << "1. 2 player friend game\n";
         std::cout << "2. vs Computer game\n";
         std::cin >> ch;
+        std::cin.ignore(); 
+
         switch (ch)
         {
         case 1:
             std::cout << "\nEnter Player 1 name: ";
-            std::cin.ignore(); 
             std::getline(std::cin, player1name);
-            p1 = new Player(player1name, mv1);
+            p1 = std::make_unique<Player>(player1name, mv1);
+
             std::cout << "\nEnter Player 2 name: ";
-            std::cin.ignore(); 
             std::getline(std::cin, player2name);
-            p2 = new Player(player2name, mv2);
+            p2 = std::make_unique<Player>(player2name, mv2);
             break;
+
         case 2:
             isAIPlayer = true;
             std::cout << "\nEnter Player 1 name: ";
-            std::cin.ignore(); 
             std::getline(std::cin, player1name);
-            p1 = new Player(player1name, mv1);
-            p2 = new AIPlayer(mv2);
+            p1 = std::make_unique<Player>(player1name, mv1);
+            p2 = std::make_unique<AIPlayer>(mv2);
             break;
 
         default:
             std::cout << "\nInvalid Player choice\n";
+            continue;
         }
-        if (ch == 1 || ch == 2)
-            break;
+        break;
     }
 
-    TickTackToe game = TickTackToe(p1, p2, isAIPlayer);
+    TickTackToe game(p1.get(), p2.get(), isAIPlayer);
     int chance = 1, played_by = 1, x, y;
     game.display();
+
     while (!game.isGameOver())
     {
-        std::cout << "\n"
-                  << game.getCurrentPlayer()->getName() << "'s turn ("
-                  << game.getCurrentPlayer()->getMove() << "):\n";
+        std::cout << "\n" << game.getCurrentPlayer()->getName() << "'s turn ("
+                  << game.getCurrentPlayer()->getMove() << "): ";
+
         while (played_by)
         {
             std::cin >> x >> y;
             chance = game.playMove(x, y);
-            if (chance == 0)
-            {
-                continue;
-            }
-            else
-            {
-                break;
-            }
+            if (chance == 0) continue;
+            else break;
         }
+
         while (!played_by)
         {
-            if (isAIPlayer && !played_by)
+            if (isAIPlayer)
             {
-                auto ai = dynamic_cast<AIPlayer *>(p2);
+                auto ai = dynamic_cast<AIPlayer*>(p2.get());
                 std::pair<int, int> aiMove = ai->getBestMove(game);
                 std::cout << "\nComputer plays: " << aiMove.first << " " << aiMove.second << "\n";
                 chance = game.playMove(aiMove.first, aiMove.second);
@@ -82,15 +81,11 @@ int main()
                 std::cin >> x >> y;
                 chance = game.playMove(x, y);
             }
-            if (chance == 0)
-            {
-                continue;
-            }
-            else
-            {
-                break;
-            }
+
+            if (chance == 0) continue;
+            else break;
         }
+
         played_by ^= 1;
     }
 
--- src/AIPlayer.cpp
+++ src/AIPlayer.cpp
@@ -3,20 +3,25 @@
 
 AIPlayer::AIPlayer(const char &move) : Player("Computer", move) {}
 
-std::pair<int, int> AIPlayer::getBestMove(Board board) const {
+std::pair<int, int> AIPlayer::getBestMove(Board board) const
+{
     int bestScore = INT_MIN;
     std::pair<int, int> bestMove = {-1, -1};
     char playerSymbol = (this->getMove() == 'X') ? 'O' : 'X';
     int boardSize = board.getBoardSize();
 
-    for (int i = 0; i < boardSize; ++i) {
-        for (int j = 0; j < boardSize; ++j) {
-            if (board.isCellEmpty(i, j)) {
+    for (int i = 0; i < boardSize; ++i)
+    {
+        for (int j = 0; j < boardSize; ++j)
+        {
+            if (board.isCellEmpty(i, j))
+            {
                 board.placeMove(i, j, this->getMove());
                 int score = minimax(board, false, this->getMove(), playerSymbol);
                 board.undoMove(i, j);
 
-                if (score > bestScore) {
+                if (score > bestScore)
+                {
                     bestScore = score;
                     bestMove = {i, j};
                 }
@@ -26,19 +31,23 @@ std::pair<int, int> AIPlayer::getBestMove(Board board) const {
     return bestMove;
 }
 
-int AIPlayer::minimax(Board& board, bool isMax, char aiSymbol, char playerSymbol) const {
+int AIPlayer::minimax(Board &board, bool isMax, char aiSymbol, char playerSymbol) const
+{
 
-    for (int i = 0; i < board.getBoardSize(); ++i) {
+    for (int i = 0; i < board.getBoardSize(); ++i)
+    {
         // Check rows
         if (board.getBoard()[i][0] != '.' &&
             board.getBoard()[i][0] == board.getBoard()[i][1] &&
-            board.getBoard()[i][1] == board.getBoard()[i][2]) {
+            board.getBoard()[i][1] == board.getBoard()[i][2])
+        {
             return board.getBoard()[i][0] == aiSymbol ? 10 : -10;
         }
         // Check columns
         if (board.getBoard()[0][i] != '.' &&
             board.getBoard()[0][i] == board.getBoard()[1][i] &&
-            board.getBoard()[1][i] == board.getBoard()[2][i]) {
+            board.getBoard()[1][i] == board.getBoard()[2][i])
+        {
             return board.getBoard()[0][i] == aiSymbol ? 10 : -10;
         }
     }
@@ -46,24 +55,30 @@ int AIPlayer::minimax(Board& board, bool isMax, char aiSymbol, char playerSymbol
     // Check diagonals
     if (board.getBoard()[0][0] != '.' &&
         board.getBoard()[0][0] == board.getBoard()[1][1] &&
-        board.getBoard()[1][1] == board.getBoard()[2][2]) {
+        board.getBoard()[1][1] == board.getBoard()[2][2])
+    {
         return board.getBoard()[0][0] == aiSymbol ? 10 : -10;
     }
 
     if (board.getBoard()[0][2] != '.' &&
         board.getBoard()[0][2] == board.getBoard()[1][1] &&
-        board.getBoard()[1][1] == board.getBoard()[2][0]) {
+        board.getBoard()[1][1] == board.getBoard()[2][0])
+    {
         return board.getBoard()[0][2] == aiSymbol ? 10 : -10;
     }
 
     // Draw
-    if (board.isBoardFull()) return 0;
+    if (board.isBoardFull())
+        return 0;
 
-    int bestScore = isMax ?  INT_MIN : INT_MAX;
+    int bestScore = isMax ? INT_MIN : INT_MAX;
 
-    for (int i = 0; i < board.getBoardSize(); ++i) {
-        for (int j = 0; j < board.getBoardSize(); ++j) {
-            if (board.isCellEmpty(i, j)) {
+    for (int i = 0; i < board.getBoardSize(); ++i)
+    {
+        for (int j = 0; j < board.getBoardSize(); ++j)
+        {
+            if (board.isCellEmpty(i, j))
+            {
                 board.placeMove(i, j, isMax ? aiSymbol : playerSymbol);
                 int score = minimax(board, !isMax, aiSymbol, playerSymbol);
                 board.undoMove(i, j);
